b258e16b6c64a8870a8d36791cf6280faa50b871
QASchoolUSA <qaschoolusa@gmail.com>
2024-05-30 19:27:32 -0400

    Added new changes

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -1,7 +1,9 @@
-import { browser } from '@wdio/globals'
+import { browser } from "@wdio/globals";
 
 export default class Page {
-    public open () {
-        return browser.url("/")
-    }
+  public open() {
+    return browser.url(
+      "https://calculator-app-mahi-gaming-interview.vercel.app/?_vercel_share=K24lAcYrl8rnc3uIUItfoA3VSCe9vdOt",
+    );
+  }
 }
